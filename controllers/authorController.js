9b6166c3bad53f8abeabd9a9e1518cf06ed0a5c7
ableRex358 <erno.soinila@gmail.com>
2023-05-13 22:46:29 +0300

    Author view implemented

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,8 +3,13 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author list");
-});
+    const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
+    res.render("author_list", {
+      title: "Author List",
+      author_list: allAuthors,
+    });
+  });
+  
 
 // Display detail page for a specific Author.
 exports.author_detail = asyncHandler(async (req, res, next) => {
