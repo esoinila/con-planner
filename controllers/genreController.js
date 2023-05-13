ac9707aada7b5a5d23295e6edf0f2ca7c249ef86
ableRex358 <erno.soinila@gmail.com>
2023-05-13 23:37:41 +0300

    More views

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,10 +1,6 @@
 const Genre = require("../models/genre");
 const asyncHandler = require("express-async-handler");
-
-// Display list of all Genre.
-/*exports.genre_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
-});*/
+const Book = require("../models/book");
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -17,9 +13,25 @@ exports.genre_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
-});
-
+    // Get details of genre and all associated books (in parallel)
+    const [genre, booksInGenre] = await Promise.all([
+      Genre.findById(req.params.id).exec(),
+      Book.find({ genre: req.params.id }, "title summary").exec(),
+    ]);
+    if (genre === null) {
+      // No results.
+      const err = new Error("Genre not found");
+      err.status = 404;
+      return next(err);
+    }
+  
+    res.render("genre_detail", {
+      title: "Genre Detail",
+      genre: genre,
+      genre_books: booksInGenre,
+    });
+  });
+  
 // Display Genre create form on GET.
 exports.genre_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Genre create GET");
