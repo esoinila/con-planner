38bbf2b28928ef1d82449502fbe0fee2cde252d8
ableRex358 <erno.soinila@gmail.com>
2023-05-15 23:57:21 +0300

    GCP-Atlas connection tinkering

--- app.js
+++ app.js
@@ -44,9 +44,11 @@ app.use(
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const dev_db_url = "mongodb://localhost:27017/local_library";
+//const dev_db_url = "mongodb+srv://test123:BNeJqYVuUo8kftXU@cluster36153.4bkgmv1.mongodb.net/myFirstDatabase?retryWrites=true";
 
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+//const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+const mongoDB = "mongodb+srv://test123:BNeJqYVuUo8kftXU@cluster36153.4bkgmv1.mongodb.net/myFirstDatabase?retryWrites=true";
 
 main().catch((err) => console.log(err));
 async function main() {
