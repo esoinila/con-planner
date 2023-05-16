077246b3543b873cb6f8552719cec316988e1e81
ableRex358 <erno.soinila@gmail.com>
2023-05-16 19:10:08 +0300

    Added model for booking

---
+++ models/booking.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+
+const Schema = mongoose.Schema;
+
+const BookingSchema = new Schema({
+  book: { type: Schema.Types.ObjectId, ref: "Game", required: true }, // reference to the associated game
+  playername: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+// Virtual for bookinstance's URL
+BookingSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/con/booking/${this._id}`;
+});
+
+BookingSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.date.toLocaleString(DateTime.DATE_MED));
+});
+
+
+// Export model
+module.exports = mongoose.model("Booking", BookingSchema);
