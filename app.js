a64cb029ffe457395b3d5b6870976f7a8bc18c12
ableRex358 <erno.soinila@gmail.com>
2023-05-16 18:06:35 +0300

    Let's use env's for db-string

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ mongoose.set("strictQuery", false);
 
 //const dev_db_url = "mongodb+srv://test123:BNeJqYVuUo8kftXU@cluster36153.4bkgmv1.mongodb.net/myFirstDatabase?retryWrites=true";
 
-//const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-const mongoDB = "mongodb+srv://test123:BNeJqYVuUo8kftXU@cluster36153.4bkgmv1.mongodb.net/myFirstDatabase?retryWrites=true";
+//const mongoDB = "mongodb+srv://test123:BNeJqYVuUo8kftXU@cluster36153.4bkgmv1.mongodb.net/myFirstDatabase?retryWrites=true";
 
 main().catch((err) => console.log(err));
 async function main() {
