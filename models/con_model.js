38511d220e59227071343997077656ff4f5ef49a
ableRex358 <erno.soinila@gmail.com>
2023-05-24 23:42:41 +0300

    Better con list now

--- models/con_model.js
+++ models/con_model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+
 
 const ConSchema = new Schema({
     title: { type: String, required: true },
