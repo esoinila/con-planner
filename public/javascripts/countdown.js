c0c14e28fa017be7c161268d443e5e476ced4d9d
ableRex358 <erno.soinila@gmail.com>
2023-05-26 14:05:41 +0300

    did stuff for countdown timer ```

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -2,9 +2,18 @@
 // I will use this time to change a text on the webpage and update it to create a countdown effect.
 // display the results in days, hours, minutes and seconds.
 
-const countdown = (countDownDate, text_id_in_dom) => {
+const countdown = (dateTextId, countdownTextId) => {
     
+    const dateTextElement = document.getElementById(dateTextId);
+    const countDownTextElement = document.getElementById(countdownTextId);
     //const countDownDate = new Date(date).getTime();
+    
+    if(dateTextElement === null || countDownTextElement === null) 
+    {
+        return;
+    }
+
+    const countDownDate = new Date(dateTextElement.innerHTML).getTime();
 
     const x = setInterval(
         function () {
@@ -15,14 +24,21 @@ const countdown = (countDownDate, text_id_in_dom) => {
             const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // hours
             const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); // minutes
             const seconds = Math.floor((distance % (1000 * 60)) / 1000); // seconds
-            document.getElementById(text_id_in_dom).innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            countDownTextElement.innerHTML.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            
             if (distance < 0) {
                 clearInterval(x);
-                document.getElementById(text_id_in_dom).innerHTML = "EXPIRED";
+                countDownTextElement.innerHTML = "EXPIRED";
             }
         }, 1000);
 
 }
+
+// create Date that is 5 days from now
+const date = new Date(Date.now() + 5 * 24 * 60 * 60 * 1000);
+countdown("countdowntime", "countdowntext");
+
+//countdown(con.date, "countdowntext");
 // countdown("Jan 5, 2021 15:37:25", "demo"); // test
 // countdown("Jan 5, 2021 15:37:25", "demo"); // test
 // countdown("Jan 5, 2021 15:37:25", "demo"); // test
