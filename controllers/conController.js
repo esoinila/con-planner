fa77e8b386843e60d8f9f9805227dc7790712bcb
ableRex358 <erno.soinila@gmail.com>
2023-06-02 18:39:11 +0300

    More unecaping form content fields

--- controllers/conController.js
+++ controllers/conController.js
@@ -313,38 +313,6 @@ exports.con_update_get = asyncHandler(async (req, res, next) => {
 
 });
 
-exports.book_update_get = asyncHandler(async (req, res, next) => {
-  // Get book, authors and genres for form.
-  const [book, allAuthors, allGenres] = await Promise.all([
-    Book.findById(req.params.id).populate("author").populate("genre").exec(),
-    Author.find().exec(),
-    Genre.find().exec(),
-  ]);
-
-  if (book === null) {
-    // No results.
-    const err = new Error("Book not found");
-    err.status = 404;
-    return next(err);
-  }
-
-  // Mark our selected genres as checked.
-  for (const genre of allGenres) {
-    for (const book_g of book.genre) {
-      if (genre._id.toString() === book_g._id.toString()) {
-        genre.checked = "true";
-      }
-    }
-  }
-
-  res.render("book_form", {
-    title: "Update Book",
-    authors: allAuthors,
-    genres: allGenres,
-    book: book,
-  });
-});
-
 
 /*
  exports.con_update_post = asyncHandler(async (req, res, next) => {
--- public/javascripts/fillTextArea.js
+++ public/javascripts/fillTextArea.js
@@ -11,6 +11,17 @@ function fillTextArea() {
     let unescapedString = unescapeHTML(textAreaValue);
     //console.log("Un-escaped string for form field: " + unescapedString);
     textArea.innerHTML = unescapedString;
-}   
+}
+
+function unescapeStringInTitleForm() {
+    var textField = document.getElementById("title");
+    var textFieldValue = document.getElementById("title").value;
+    let unescapedString = unescapeHTML(textFieldValue);
+    //console.log("Un-escaped string for text-form field: " + unescapedString);
+    textField.value = unescapedString;
+}
+
 
 fillTextArea();
+
+unescapeStringInTitleForm();
