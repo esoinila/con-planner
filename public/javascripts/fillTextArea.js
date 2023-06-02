7cf5a2d9a2486b0629fbc4ec3413272ee62090f6
ableRex358 <erno.soinila@gmail.com>
2023-06-02 16:36:33 +0300

    cleaning up

--- public/javascripts/fillTextArea.js
+++ public/javascripts/fillTextArea.js
@@ -1,16 +1,9 @@
-const escapedString = 'single escaped quote next &#x27;';
 
 function unescapeHTML(escapedHTML) {
     var d = document.createElement('div');
     d.innerHTML = escapedHTML;
     return d.textContent;
 }
-
-console.log("escaped: " + escapedString); // This is an escaped string &#x27;
-const unescapedString = unescapeHTML(escapedString);
-console.log("unescaped: " + unescapedString); // This is an unescaped string &#x27;
-
-
 
 function fillTextArea() {
     var textArea = document.getElementById("description");
